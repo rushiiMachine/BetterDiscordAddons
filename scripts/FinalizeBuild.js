8855b252fbe845f0bce5979710b594b5fcaaa724
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-12-07 19:52:10 -0800

    build: install script

--- scripts/FinalizeBuild.js
+++ scripts/FinalizeBuild.js
@@ -52,9 +52,31 @@ buffer += '\n*/\n';
 /* Add licenses */
 config.build.licenses?.map(license => (buffer += `/** ${license} **/\n`));
 
+if (config.build.includeInstallScript) buffer += `
+/*@cc_on
+@if (@_jscript)
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\\\BetterDiscord\\\\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	shell.Popup("Moving plugin to BetterDiscord's plugin folder...", 0, "${config.info.name}", 0x40);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("Plugin already installed!", 0, "${config.info.name}", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("Failed to find BetterDiscord!\\nIs it even installed?", 0, "${config.info.name}", 0x10);
+	}
+	fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+	shell.Popup("Successfully installed!", 0, "${config.info.name}", 0x40);
+	WScript.Quit();
+@else@*/
+`
+
 /* Add code */
 buffer += fs
   .readFileSync(pluginFile, 'utf8')
   .replace(`/*! For license information please see ${pluginName}.plugin.js.LICENSE.txt */\n`, '');
+
+if (config.build.includeInstallScript) buffer += `\n/*@end@*/`
 
 fs.writeFileSync(pluginFile, buffer, { encoding: 'utf8', flag: 'w' });
